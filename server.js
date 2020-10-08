eb7bc5b8bf6d40f7fdcd2a7dd4a469ffc701b4ea
malex19889 <malex19889@yahoo.com>
2020-10-07 23:46:47 -0500

    adding working note post

--- server.js
+++ server.js
@@ -1,23 +1,36 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
+const { parse } = require("path");
 const app = express();
 const PORT = process.env.PORT || 8080;
+const util =require("util");
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
-app.use(express.static("db"));
 app.use(express.json());
+
+const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+
 app.get("/",function(req,res){
     res.sendFile(path.join(__dirname,"index.html"));
 })
+
 app.get("/notes",function (req,res) {
     res.sendFile(path.join(__dirname,"/public/notes.html"));
-});
-app.get("/api/notes",function (req,res) {
-    res.sendFile(path.join(__dirname,"/db/db.json"));
-});
-app.post("/api/notes",function (req,res) {
-    res.send('<p>some html</p>');
 })
+
+app.get("/api/notes",function (req,res) {
+    console.log(notes)
+    res.sendFile(path.join(__dirname,"./db/db.json"));
+})
+
+app.post("/api/notes",function(req,res){
+    let newNote = req.body;
+    newNote.id = notes.length;
+    notes.push(newNote);
+    fs.writeFileSync("./db/db.json",JSON.stringify(notes));
+})
+
 app.listen(PORT,function(){
     console.log("server listening on "+PORT);
 });
